1b52245cc2fea9ab4437a8a15ffe84ec1c6c88bd
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2022-12-26 16:19:45 +0200

    second

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
+import Debounce from "lodash.debounce";
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    inputEl: document.querySelector('#search-box'),
+    listEl: document.querySelector('.country-list'),
+    infoEl: document.querySelector('.country-info')
+}
+
+refs.inputEl.addEventListener('input', Debounce((e) => {
+    refs.listEl.innerHTML = '';
+    refs.infoEl.innerHTML = '';
+    const value = e.target.value.trim();
+    if (value === '') {
+        refs.listEl.innerHTML = '';
+        refs.infoEl.innerHTML = '';
+    } else {
+        renderCountries(value);
+    }
+},DEBOUNCE_DELAY))
+
+function createMurkupList(countries) {
+    const markup = countries.map(country => {
+        return `
+            <li>
+                <img src="${country.flags.svg}" alt="flag">
+                <h1>${country.name.official}</h1>
+            </li>
+            `
+    }).join('');
+
+    return markup;
+}
+
+function createMurkupInfo(countries) {
+    const markup = countries.map(country => {
+        return `
+            <li>
+            <img src="${country.flags.svg}" alt="flag">
+            <h1>${country.name.official}</h1>
+            </li>
+            <p><span>Capital:</span> ${country.capital}</p>
+            <p><span>Population:</span> ${country.population}</p>
+            <p><span>Languages:</span> ${Object.values(country.languages).join(' ')}</p >
+            `
+    }).join('');
+    return markup;
+}
+
+function renderCountries(value) {
+        fetchCountries(value)
+        .then(data => {
+    if (data.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (data.length > 1 && data.length <= 10 ) {
+        refs.listEl.innerHTML = createMurkupList(data);
+    } else if (data.length === 1) {
+        refs.infoEl.innerHTML = createMurkupInfo(data)
+    }
+        })
+        .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+    })
+}
